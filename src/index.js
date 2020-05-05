00f8ce66423ae73abc82241b1126eaf16cc021db
rachit2501 <rac.sri25@gmail.com>
2020-05-05 18:08:30 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-import nav from "./nav";
-// import { footer } from "./footer";
-const gsap = ()=>import('gsap');
-const getFooter = ()=>import(/* webpackChunkName: "footer" */ "./footer");
-
-import makeButton from "./button";
-import { makeColorStyle } from "./button-styles";
-import makeImage from "./image";
-import imageUrl from "./webpack-logo.jpg";
-import css from "./footer.css";
-import buttonStyles from "./button.css";
-
-const image = makeImage(imageUrl);
-const button = makeButton("Yay! A Button!");
-button.style = makeColorStyle("cyan");
-
-document.body.appendChild(button);
-const setButtonStyles = (style)=>import(`./buttonStyles/${style}`)
-
-button.addEventListener('click',e=>{
-    getFooter().then(footerModule=>{
-        document.body.appendChild(footerModule.footer)
-    })
-    gsap().then(g=>console.log(g))
-
-    setButtonStyles("red").then(s=>{
-        debugger;
-        button.style =s.default
-    })
-})
-document.body.appendChild(image);
+import App from "./App.tsx";
+console.log(App);
+import css from "./style.css";
+console.log(css);
